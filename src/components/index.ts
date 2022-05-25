9c8028371f761e8c00b2bac2ccd2b92e70ccd1c1
guapuken <44274367+guapuken@users.noreply.github.com>
2022-05-25 17:55:24 -0500

    TextField-Agregado

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {default as Logo} from './Logo';
 export {default as DynamicButton} from './DynamicButton';
 export {default as Inputs}from './Inputs';
+export {default as TextField} from './TextField';
