cf06c4d07180a6a62e08b4966fa7d238f9ee4031
guapuken <44274367+guapuken@users.noreply.github.com>
2022-05-25 18:18:28 -0500

    Add-Titles-Cards

---
+++ src/components/Atomos/TitlesCards/TitlesCards.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './index.css'
+import '../../../index.css'
+
+const getModo = (modo:any) => {
+    switch (modo) {
+        case 'Dark': return('DarkMode')
+        default: return ''
+    }
+}
+
+interface TitlesCardsProps{
+    clave?:string | any;
+    type?: string | any;
+    modo?: string | any;
+}
+const TitlesCards = (props:TitlesCardsProps) => {
+    const clave = props.clave
+    const type = props.type
+    return(
+        <div>
+            <h2 className={"ClaveCards"+getModo(props.modo)}>
+                {clave}
+            </h2>
+            <h3 className={"TipoCards"+getModo(props.modo)}>
+                {type}
+            </h3>
+        </div>
+    )
+}
+
+export default TitlesCards;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export {default as DynamicButton} from './Atomos/DynamicButton';
 export {default as Inputs}from './Atomos/Inputs';
 export {default as TextField} from './Atomos/TextField';
 export {default as PropertyBtn} from './Atomos/PropertyBtn';
+export {default as TitlesCards}from './Atomos/TitlesCards';
