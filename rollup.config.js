03356e659662aca08c9e2020210bbabe66665fd8
jacorrea <jcorrea@e-personal.net>
2022-05-30 08:45:11 -0500

    Cambios en configuración de rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
 import dts from "rollup-plugin-dts";
 
 import postcss from 'rollup-plugin-postcss';
@@ -11,12 +12,7 @@ export default [
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
+        file: "dist/app.js",
         format: "esm",
         sourcemap: true,
       },
@@ -26,12 +22,11 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      copy({
+        targets: [
+          { src: "src/img/**/*", dest: "dist/img" }
+        ]
+      })
     ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external:[/\.css$/],
-  },
+  }
 ];
