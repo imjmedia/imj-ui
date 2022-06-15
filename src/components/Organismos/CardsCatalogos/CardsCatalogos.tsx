60337b00e4afab57fb11f44f9326165889fc6ccf
guapuken <44274367+guapuken@users.noreply.github.com>
2022-06-15 18:13:05 -0500

    Add Aside Flexible en CardsCatalogos

--- src/components/Organismos/CardsCatalogos/CardsCatalogos.tsx
+++ src/components/Organismos/CardsCatalogos/CardsCatalogos.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import DynamicButton from "../../Atomos/DynamicButton";
 import '../../../index.css';
 import { getColorMedio, getModo } from "../../../utils/utils";
@@ -7,15 +7,17 @@ import { getColorMedio, getModo } from "../../../utils/utils";
 interface CardsCatalogosProps {
     frontItemRenderer: any,
     backItemRenderer?: any,
+    asideItemRenderer?: any;
     modo?: string | any,
     data: any,
     handleClose?: () => void;
 }
 const CardsCatalogos = (props: CardsCatalogosProps) => {
+    const Botones = props.asideItemRenderer;
     const Front = props.frontItemRenderer;
     const Back = props.backItemRenderer;
     const modoCard = getModo(props.modo || '');
-    const FlipNoFlip = (lado:string) => {
+    const Flip = (lado:string) => {
         if (Front && Back){
             if (lado === 'front'){ 
                 return('CardFront') 
@@ -29,10 +31,10 @@ const CardsCatalogos = (props: CardsCatalogosProps) => {
         <div className="CardsPaddings">
             <div className="CardAsideFlex">
                 <div className="ContenedorCards">
-                    <div className={FlipNoFlip('front') + " Card" + modoCard}>
+                    <div className={Flip('front') + " Card" + modoCard}>
                         <Front data={props.data}/>
                     </div>
-                    {Back && <div className={FlipNoFlip('back') + " Card" + modoCard}>
+                    {Back && <div className={Flip('back') + " Card" + modoCard}>
                         <Back data={props.data}/>
                     </div>}
                     <div className="EtiquetaIdentificacion">
@@ -44,9 +46,43 @@ const CardsCatalogos = (props: CardsCatalogosProps) => {
                         />
                     </div>
                 </div>
+                <div>
+                    {Botones}
+                </div>
             </div>
         </div>
     )
 }
+/* 
+Creo que esto sería lo que va en el archivo externo que retorna el asideItemRenderer:
+
+interface asideItemRendererProps{
+    modo?: 'Dark' | '';
+}
+
+export const asideItemRenderer =(props:asideItemRendererProps)=>{
+    const modoCard = getModo(props.modo || '')
+    const Btn ={
+        btn1: (
+                <BtnFunction
+                    button="vaciar"
+                    color="Rojo"
+                    onClick={()=>{}}
+                />    
+        )
+    }
+    if (Btn != null){
+        return(
+            <div className={"AsideCards" + modoCard}>
+                <div className="BotonAside">
+                    {Btn.btn1}    
+                </div>       
+            </div> 
+         )
+    } else {
+        return <></>
+    }
+}
+*/
 
 export default CardsCatalogos;
